022314978a572336413b800ef665980c66ae39a3
John.Linder <john.artur.linder@gmail.com>
2020-09-23 14:50:27 +0200

    fixed

--- thetests.py
+++ thetests.py
@@ -66,7 +66,7 @@ def gettodaysdatewith_zeroformats():
 fullDayAbsenceRequestID = '8e793f5a-b4c8-40c1-9156-ac3d00bb51a9'
 overTimeRequestId = '30fadb41-90b5-48dd-832f-ac3e00c0e8a7'
 
-
+'''
 @pytest.mark.command
 @pytest.mark.queries
 @pytest.mark.runfirst
@@ -107,7 +107,7 @@ def test_seleniumtogetAPItoken():
     driver.close()
     global apitoken
     apitoken = apitoken2
-
+'''
 #these test are commands
 with open('csvtestdata/test_AddFullDayAbsence.csv') as f:
  reader = csv.reader(f)
@@ -136,6 +136,7 @@ def test_AddFullDayAbsence(AbsenceId,ScenarioId):
          print(response.text.encode('utf8'))
 
      assert response.status_code == 200
+
 with open('csvtestdata/test_AddFullDayAbsenceRequest.csv') as f:
     reader = csv.reader(f)
     dataaddfulldayabsencerequest = list(reader)
@@ -173,6 +174,7 @@ def test_AddFullDayAbsenceRequest(AbsenceId,Subject):
     fullDayAbsenceRequestID = o.get("Id")
 
     assert response.status_code == 200
+
 with open('csvtestdata/test_AddIntradayAbsenceRequest.csv') as f:
     reader = csv.reader(f)
     intraRequestData = list(reader)
@@ -204,6 +206,7 @@ def test_AddIntradayAbsenceRequest(Subject,Message,TimeZoneID,AbsenceID):
         print(response.text.encode('utf8'))
 
     assert response.status_code == 200
+
 with open('csvtestdata/addorremovemeeting.csv') as f:
  reader = csv.reader(f)
  dataforaddmeeting = list(reader)
@@ -248,6 +251,29 @@ def test_AddMeeting(ExternalMeetingId, Title, StartMeetingHour, EndMeetingHour, 
         print(response.text.encode('utf8'))
 
     assert response.status_code == 200
+
+@pytest.mark.command
+@pytest.mark.parametrize("ExternalMeetingId", dataforremovemeeting)
+def test_RemoveMeeting(ExternalMeetingId):
+    requestdata = {
+  "ExternalMeetingId": ExternalMeetingId
+}
+    payload = json.dumps(requestdata)
+    headers = {
+        'Authorization': apitoken,
+        'Content-Type': 'application/json'
+    }
+
+    try:
+        response = requests.request("POST", baseurl + "/command/RemoveMeeting", headers=headers, data=payload)
+
+    except requests.exceptions.ConnectionError:
+        print("Internet connection down")
+    else:
+        print(response.text.encode('utf8'))
+
+    assert response.status_code == 200
+
 with open('csvtestdata/test_AddOvertimeRequest.csv') as f:
     reader = csv.reader(f)
     overTimeRequestData = list(reader)
@@ -344,6 +370,7 @@ def test_AddScheduleChangesListener():
         print(response.text.encode('utf8'))
 
     assert response.status_code == 200
+
 @pytest.mark.command
 def test_AddTeam():
     requestdata = {
@@ -366,27 +393,7 @@ def test_AddTeam():
         print(response.text.encode('utf8'))
 
     assert response.status_code == 200
-@pytest.mark.command
-@pytest.mark.parametrize("ExternalMeetingId", dataforremovemeeting)
-def test_RemoveMeeting(ExternalMeetingId):
-    requestdata = {
-  "ExternalMeetingId": ExternalMeetingId
-}
-    payload = json.dumps(requestdata)
-    headers = {
-        'Authorization': apitoken,
-        'Content-Type': 'application/json'
-    }
 
-    try:
-        response = requests.request("POST", baseurl + "/command/RemoveMeeting", headers=headers, data=payload)
-
-    except requests.exceptions.ConnectionError:
-        print("Internet connection down")
-    else:
-        print(response.text.encode('utf8'))
-
-    assert response.status_code == 200
 with open('csvtestdata/test_RemovePersonAbsence.csv') as f:
     reader = csv.reader(f)
     RemovePersonAbsenceData = list(reader)
@@ -399,7 +406,7 @@ def test_RemovePersonAbsence(ScenarioId, TimeZoneId):
   "PersonId": "B0E35119-4661-4A1B-8772-9B5E015B2564",
   "Period": {
     "StartTime": gettodaysdatewith_zeroformats()+"T08:10:09.155Z",
-    "EndTime": getdate30daysahead_zeroformats()+"T16:10:09.155Z"
+    "EndTime": getdate30daysahead_zeroformats()+"T08:10:09.155Z"
   },
   "ScenarioId": ScenarioId
 }
@@ -418,6 +425,7 @@ def test_RemovePersonAbsence(ScenarioId, TimeZoneId):
         print(response.text.encode('utf8'))
 
     assert response.status_code == 200
+
 with open('csvtestdata/test_SetSchedulesForPerson.csv') as f:
     reader = csv.reader(f)
     SetSchedulesForPersonData = list(reader)
@@ -433,15 +441,15 @@ def test_SetSchedulesForPerson(ScenarioId,TimeZoneId,ActivityId):
   },
   "ScheduleDays": [
     {
-      "Date": getdate30daysahead_zeroformats(),
+      "Date": getdate31daysahead_zeroformats(),
       "ShiftCategoryId": "EC7F6475-1AE7-4A94-83C1-9B5E015AB4C3",
       #"DayOffTemplateId": "F2F35A09-9453-4944-A3F7-550DC06EA0FF",
       #"FullDayAbsenceId": "47D9292F-EAD6-40B2-AC4F-9B5E015AB330",
       "Layers": [
         {
           "Period": {
-            "StartTime": getdate30daysahead_zeroformats()+"T08:18:00.000",
-            "EndTime": getdate30daysahead_zeroformats()+"T16:18:00.000"
+            "StartTime": getdate31daysahead_zeroformats()+"T08:18:00.000",
+            "EndTime": getdate31daysahead_zeroformats()+"T16:18:00.000"
           },
           "ActivityId": ActivityId,
           #"AbsenceId": "47D9292F-EAD6-40B2-AC4F-9B5E015AB330"
@@ -488,7 +496,7 @@ def test_SetSchedulesForPerson_MultipleLayersOfActivitysWithinDateAndMultipleSch
         {
           "Period": {
             "StartTime": getdate30daysahead_zeroformats()+"T08:18:00.000",
-            "EndTime": getdate31daysahead_zeroformats()+"T16:18:00.000"
+            "EndTime": getdate30daysahead_zeroformats()+"T10:18:00.000"
           },
           "ActivityId": ActivityId,
           #"AbsenceId": "47D9292F-EAD6-40B2-AC4F-9B5E015AB330"
@@ -520,7 +528,7 @@ def test_SetSchedulesForPerson_MultipleLayersOfActivitysWithinDateAndMultipleSch
               {
                   "Period": {
                       "StartTime": getdate32daysahead_zeroformats() + "T08:18:00.000",
-                      "EndTime": getdate32daysahead_zeroformats() + "T16:18:00.000"
+                      "EndTime": getdate32daysahead_zeroformats() + "T10:18:00.000"
                   },
                   "ActivityId": "BA3624B0-0AEA-4B72-A585-9B5E015AB3C6",
                   # "AbsenceId": "47D9292F-EAD6-40B2-AC4F-9B5E015AB330"
@@ -552,7 +560,7 @@ def test_SetSchedulesForPerson_MultipleLayersOfActivitysWithinDateAndMultipleSch
               {
                   "Period": {
                       "StartTime": getdate34daysahead_zeroformats() + "T08:18:00.000",
-                      "EndTime": getdate34daysahead_zeroformats() + "T16:18:00.000"
+                      "EndTime": getdate34daysahead_zeroformats() + "T10:18:00.000"
                   },
                   "ActivityId": "92159C8B-69B9-4F16-AD18-9B5E015AB3C6",
                   # "AbsenceId": "47D9292F-EAD6-40B2-AC4F-9B5E015AB330"
@@ -584,7 +592,7 @@ def test_SetSchedulesForPerson_MultipleLayersOfActivitysWithinDateAndMultipleSch
               {
                   "Period": {
                       "StartTime": getdate36daysahead_zeroformats() + "T08:18:00.000",
-                      "EndTime": getdate36daysahead_zeroformats() + "T16:18:00.000"
+                      "EndTime": getdate36daysahead_zeroformats() + "T10:18:00.000"
                   },
                   "ActivityId": ActivityId,
                   # "AbsenceId": "47D9292F-EAD6-40B2-AC4F-9B5E015AB330"
